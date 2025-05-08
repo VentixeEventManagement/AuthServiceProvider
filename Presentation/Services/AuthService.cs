2127a48eea864fd71c054b425c2b9c363dd87133
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-08 09:17:16 +0200

    SignUpAsync method created. SignUpResult and SignInForm created

--- Presentation/Services/AuthService.cs
+++ Presentation/Services/AuthService.cs
@@ -1,5 +1,29 @@
-﻿namespace Presentation.Services;
+﻿using Presentation.Models;
 
-public class AuthService
+namespace Presentation.Services;
+
+public class AuthService(AccountGrpcService.AccountGrpcServiceClient accountClient)
 {
+    private readonly AccountGrpcService.AccountGrpcServiceClient _accountClient = accountClient;
+
+    public async Task<SignUpResult> SignUpAsync(SignUpForm formData)
+    {
+        try
+        {
+            var request = new CreateAccountRequest
+            {
+                Email = formData.Email,
+                Password = formData.Password,
+            };
+
+            var reply = await _accountClient.CreateAccountAsync(request);
+            return reply.Succeeded
+                ? new SignUpResult { Succeeded = reply.Succeeded, Message = reply.Message, UserId = reply.UserId }
+                : new SignUpResult { Succeeded = reply.Succeeded, Message = reply.Message };
+        }
+        catch (Exception ex)
+        {
+            return new SignUpResult { Succeeded = false, Message = ex.Message };
+        }
+    }
 }
