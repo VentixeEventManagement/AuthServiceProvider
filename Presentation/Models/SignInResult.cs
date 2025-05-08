0492477b7a9d4edc590ca4301409b6c0644741e6
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-08 09:20:26 +0200

    SignInAsync method created. SignInResult and SignInForm created.

---
+++ Presentation/Models/SignInResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentation.Models;
+
+public class SignInResult
+{
+    public bool Succeeded { get; set; }
+    public string? Message { get; set; }
+    public string? UserId { get; set; }
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
--- Presentation/Services/AuthService.cs
+++ Presentation/Services/AuthService.cs
@@ -26,4 +26,30 @@ public class AuthService(AccountGrpcService.AccountGrpcServiceClient accountClie
             return new SignUpResult { Succeeded = false, Message = ex.Message };
         }
     }
+
+    public async Task<SignInResult> SignInAsync(SignInForm formData)
+    {
+        try
+        {
+            var request = new ValidateCredentialsRequest
+            {
+                Email = formData.Email,
+                Password = formData.Password,
+            };
+
+            var reply = await _accountClient.ValidateCredentialsAsync(request);
+            if (!reply.Succeeded)
+            {
+                return new SignInResult { Succeeded = reply.Succeeded, Message = reply.Message };
+            }
+
+            // Generate Token
+
+            return new SignInResult { Succeeded = reply.Succeeded, Message = reply.Message };
+        }
+        catch (Exception ex)
+        {
+            return new SignInResult { Succeeded = false, Message = ex.Message };
+        }
+    }
 }
