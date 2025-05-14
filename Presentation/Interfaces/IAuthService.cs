49cb7b002fef054b4d0b70c26338db36aa5cf7a3
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-14 13:21:12 +0200

    Update IAuthService to prioritize user sign-in
    Modified method declarations in IAuthService.cs:
    - Activated the SignInAsync method, which was previously commented out.
    - Commented out the SignUpAsync method, indicating a shift in focus from user sign-up to sign-in.

--- Presentation/Interfaces/IAuthService.cs
+++ Presentation/Interfaces/IAuthService.cs
@@ -4,8 +4,8 @@ namespace Presentation.Interfaces
 {
     public interface IAuthService
     {
-        //Task<SignInResult> SignInAsync(SignInForm formData);
-        Task<SignUpResult> SignUpAsync(SignUpForm formData);
+        Task<SignInResult> SignInAsync(SignInForm formData);
+        //Task<SignUpResult> SignUpAsync(SignUpForm formData);
         Task<SignUpResult> VerificationCodeRequestAsync(string email);
         Task<SignUpResult> VerifyCodeAndCreateAccountAsync(SignInForm formData, string verificationCode);
     }
