28af6d35bda2f1d212dac0e16d35738d4d73aa68
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-14 09:57:14 +0200

    Add Azure Service Bus support with AuthServiceBusHandler
    Introduce Azure Messaging Service Bus package reference in
    `Presentation.csproj`. Add `AuthServiceBusHandler` class in
    `AuthServiceBusHandler.cs` for sending messages to Service Bus,
    including initialization and asynchronous publishing methods.

---
+++ Presentation/Services/AuthServiceBusHandler.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace Presentation.Services;
+
+public class AuthServiceBusHandler
+{
+    private readonly ServiceBusClient _client;
+    private readonly ServiceBusSender _sender;
+
+    public AuthServiceBusHandler(IConfiguration configuration)
+    {
+        _client = new ServiceBusClient(configuration["ServiceBus: ConnectionString"]);
+        _sender = _client.CreateSender(configuration["ServiceBus:AccountVerification"]);
+    }
+
+    public async Task PublishAsync(string payload)
+    {
+        var message = new ServiceBusMessage(payload);
+        await _sender.SendMessageAsync(message);
+
+        await _sender.DisposeAsync();
+        await _client.DisposeAsync();
+    }
+
+}
